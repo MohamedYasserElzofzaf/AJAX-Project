429d5c8794c15d40a3cc75e78c4e847a2108eada
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-05 13:59:11 +0200

    apper the photo on the background

--- js/script.js
+++ js/script.js
@@ -1,23 +1,30 @@
-
 function loadData() {
-
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+    var $body = $("body");
+    var $wikiElem = $("#wikipedia-links");
+    var $nytHeaderElem = $("#nytimes-header");
+    var $nytElem = $("#nytimes-articles");
+    var $greeting = $("#greeting");
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
+    var streetStr = $("#street").val();
+    var cityStr = $("#city").val();
+    var address = streetStr + ", " + cityStr;
 
+    $greeting.text("so , you want to live at " + address + "?");
+    var streetviewUrl =
+        "http://maps.googleapis.com/maps/api/streetview?size=600x300&location= " +
+        address +
+        "";
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
     // load streetview
 
     // YOUR CODE GOES HERE!
 
     return false;
-};
+}
 
-$('#form-container').submit(loadData);
+$("#form-container").submit(loadData);
 
 // loadData();
